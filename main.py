9c26d99c227759db87d2d3f09f2b377bcaba2fc6
mcbrezel <jg.jacobs@hotmail.de>
2023-10-25 12:00:41 +0200

    Plugged residuals into dataframe indexed by test x-values

--- main.py
+++ main.py
@@ -38,13 +38,27 @@ if __name__ == "__main__":
     
     # 2) Selection validation
     # Check which test data point fits which selected ideal function
-    count_xs_test = df_test.shape[0]
     selected_ideal_funcs = df_ideal.iloc[:, sum_lsq_indices]
-    test_deltas = np.full(shape=(count_xs_test, selected_ideal_funcs.shape[1]), fill_value=sys.float_info.max)
-    max_deltas = np.full(shape=selected_ideal_funcs.shape, fill_value=sys.float_info.max)
-    for col_ideal in range(selected_ideal_funcs.shape[1]):
+    count_xs_test = df_test.shape[0]
+    count_ys_sel_ideal = selected_ideal_funcs.shape[1]
+    test_deltas = np.full(shape=(count_xs_test, count_ys_sel_ideal), fill_value=sys.float_info.max)
+    train_deltas = np.full(shape=df_train.iloc[:, 1:].shape, fill_value=sys.float_info.max)
+    max_test_deltas = np.full(shape=count_ys_sel_ideal, fill_value=sys.float_info.max)
+    max_train_deltas = np.full_like(a=max_test_deltas, fill_value=sys.float_info.max)
+    
+    for col_sel_ideal in range(count_ys_sel_ideal):
         for row_test in range(count_xs_test):
             x_test, y_test = df_test.iloc[row_test, :] 
-            test_deltas[row_test, col_ideal] = (selected_ideal_funcs[df_ideal["x"] == x_test].iloc[:, col_ideal] - y_test) ** 2
-        max_deltas[col_ideal] = np.max(test_deltas[col_ideal])
-    
+            test_deltas[row_test, col_sel_ideal] = (selected_ideal_funcs[df_ideal["x"] == x_test].iloc[:, col_sel_ideal] - y_test) ** 2
+        max_test_deltas[col_sel_ideal] = np.max(test_deltas[:, col_sel_ideal])
+        train_deltas[:, col_sel_ideal] = (df_train.iloc[:, col_sel_ideal + 1] - selected_ideal_funcs.iloc[:, col_sel_ideal]) ** 2
+        max_train_deltas[col_sel_ideal] = np.max(train_deltas[:, col_sel_ideal])
+
+    # adding x-column next to the y-columns of selected ideal functions
+    df_sel_ideal = pd.DataFrame(np.hstack((np.atleast_2d(df_ideal["x"]).T, selected_ideal_funcs)), columns=np.hstack(("x", selected_ideal_funcs.columns)))
+    residuals = pd.DataFrame(data=np.full(shape=(count_xs_test, count_ys_sel_ideal), fill_value=-1), index=df_test["x"], columns=df_sel_ideal.columns[1:], dtype="float32")
+    # calculating residuals of test data compared to selected ideal functions
+    for x_test in df_test["x"]:
+        for index in np.where(df_test["x"] == x_test)[0]:
+            residuals.iloc[index] = (df_sel_ideal[df_sel_ideal["x"] == x_test].iloc[:, 1:] - df_test.loc[df_test["x"] == x_test]["y"][index]) ** 2
+    print(residuals)
