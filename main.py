17cd5b69ffede99b6d3b29e3753e216f1ecdb8c6
mcbrezel <jg.jacobs@hotmail.de>
2023-10-27 17:09:19 +0200

    Completed selection validation and added human-readable print-out of results

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pandas as pd
 from matplotlib import style, pyplot as plot
 import seaborn as sb
 import sys
+import math
+from fitting import Fitting
 
 path_train = "data/train.csv"
 path_ideal = "data/ideal.csv"
@@ -24,6 +26,7 @@ if __name__ == "__main__":
 
     # 1) Sum of Least Squares (SLS)
     # Check which ideal function best fits which training function
+    ##############################################################
     count_ys_train = df_train.shape[1] - 1
     count_ys_ideal = df_ideal.shape[1] - 1
     sum = np.empty(shape=(count_ys_train, count_ys_ideal))
@@ -38,27 +41,27 @@ if __name__ == "__main__":
     
     # 2) Selection validation
     # Check which test data point fits which selected ideal function
+    ################################################################
     selected_ideal_funcs = df_ideal.iloc[:, sum_lsq_indices]
     count_xs_test = df_test.shape[0]
     count_ys_sel_ideal = selected_ideal_funcs.shape[1]
-    test_deltas = np.full(shape=(count_xs_test, count_ys_sel_ideal), fill_value=sys.float_info.max)
+    # adding x-column next to the y-columns of selected ideal functions to enable indexing by x_test value
+    df_sel_ideal = pd.DataFrame(data=np.hstack((np.atleast_2d(df_ideal["x"]).T, selected_ideal_funcs)), columns=np.hstack(("x", selected_ideal_funcs.columns)))
     train_deltas = np.full(shape=df_train.iloc[:, 1:].shape, fill_value=sys.float_info.max)
-    max_test_deltas = np.full(shape=count_ys_sel_ideal, fill_value=sys.float_info.max)
-    max_train_deltas = np.full_like(a=max_test_deltas, fill_value=sys.float_info.max)
-    
-    for col_sel_ideal in range(count_ys_sel_ideal):
-        for row_test in range(count_xs_test):
-            x_test, y_test = df_test.iloc[row_test, :] 
-            test_deltas[row_test, col_sel_ideal] = (selected_ideal_funcs[df_ideal["x"] == x_test].iloc[:, col_sel_ideal] - y_test) ** 2
-        max_test_deltas[col_sel_ideal] = np.max(test_deltas[:, col_sel_ideal])
-        train_deltas[:, col_sel_ideal] = (df_train.iloc[:, col_sel_ideal + 1] - selected_ideal_funcs.iloc[:, col_sel_ideal]) ** 2
-        max_train_deltas[col_sel_ideal] = np.max(train_deltas[:, col_sel_ideal])
-
-    # adding x-column next to the y-columns of selected ideal functions
-    df_sel_ideal = pd.DataFrame(np.hstack((np.atleast_2d(df_ideal["x"]).T, selected_ideal_funcs)), columns=np.hstack(("x", selected_ideal_funcs.columns)))
-    residuals = pd.DataFrame(data=np.full(shape=(count_xs_test, count_ys_sel_ideal), fill_value=-1), index=df_test["x"], columns=df_sel_ideal.columns[1:], dtype="float32")
+    df_train_deltas = pd.DataFrame(data=train_deltas, index=df_train["x"], columns=selected_ideal_funcs.columns, dtype="float32")
+    df_test_deltas = pd.DataFrame(data=np.full(shape=(count_xs_test, count_ys_sel_ideal), fill_value=-1), index=df_test["x"], columns=df_sel_ideal.columns[1:], dtype="float32")
+    # list of smallest fittable deltas for each test data point for when there are multiple fits
+    df_fittable_min_delta = pd.DataFrame(data=np.empty(shape=(count_xs_test, 2)))
+    # container for final table data
+    fittings = []
     # calculating residuals of test data compared to selected ideal functions
     for x_test in df_test["x"]:
-        for index in np.where(df_test["x"] == x_test)[0]:
-            residuals.iloc[index] = (df_sel_ideal[df_sel_ideal["x"] == x_test].iloc[:, 1:] - df_test.loc[df_test["x"] == x_test]["y"][index]) ** 2
-    print(residuals)
+        for test_index in np.where(df_test["x"] == x_test)[0]:
+            df_test_deltas.iloc[test_index] = (df_sel_ideal[df_sel_ideal["x"] == x_test].iloc[:, 1:] - df_test.loc[df_test["x"] == x_test]["y"][test_index]) ** 2
+            col_indices_fittable = np.where((df_train_deltas.loc[x_test] * math.sqrt(2)) - df_test_deltas.loc[x_test] > 0)
+            df_fittable_deltas = df_test_deltas.iloc[test_index].iloc[col_indices_fittable[0]]
+            col_index_fittable_min_delta = np.where(df_test_deltas.iloc[test_index] == np.min(df_test_deltas.iloc[test_index]))
+            fittings.append(Fitting(x=x_test, y=df_test["y"].iloc[test_index], delta=df_test_deltas.iloc[test_index, col_index_fittable_min_delta[0][0]], ideal_function=df_test_deltas.columns[col_index_fittable_min_delta[0][0]]))
+
+    for f in fittings:
+        print(f)
